8fb55f83fcb652b72efcfd1cc5d1910e1308baaa
kingsonseang <kingsonseang@gmail.com>
2025-02-16 15:35:15 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,8 +231,8 @@ export default function Home() {
               <AccordionItem value='item-3'>
                 <AccordionTrigger>Is it animated?</AccordionTrigger>
                 <AccordionContent>
-                  Yes. It's animated by default, but you can disable it if you
-                  prefer.
+                  Yes. It&apos;s animated by default, but you can disable it if
+                  you prefer.
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
