90d867ccd88a429af40be336f434a22810601f2f
kingsonseang <kingsonseang@gmail.com>
2025-02-28 19:54:13 +0100

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               alt=''
               // width={400}
               // height={600}
-              className='object-cover w-full h-full z-0'
+              className='object-cover w-full h-full z-0 object-top'
               fill
             />
 
